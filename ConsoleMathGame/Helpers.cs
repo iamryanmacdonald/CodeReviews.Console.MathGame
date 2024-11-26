df24d28d95b89ca4366cf210b1143c137b4dc9b5
Ryan Macdonald <ryan@ryanmacdonald.io>
2024-11-26 17:43:48 +1100

    refactored code to use object oriented programming

---
+++ ConsoleMathGame/Helpers.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleMathGame;
+
+internal class Helpers
+{
+    private static readonly List<string> games = new();
+
+    internal static void AddToHistory(int gameScore, string gameType)
+    {
+        games.Add($"{DateTime.Now} - {gameType}: {gameScore} pts");
+    }
+
+    internal static int[] GetDivisionNumbers()
+    {
+        var random = new Random();
+        var firstNumber = random.Next(1, 99);
+        var secondNumber = random.Next(1, 99);
+
+        var result = new int[2];
+
+        while (firstNumber % secondNumber != 0)
+        {
+            firstNumber = random.Next(1, 99);
+            secondNumber = random.Next(1, 99);
+        }
+
+        result[0] = firstNumber;
+        result[1] = secondNumber;
+
+        return result;
+    }
+
+    internal static void GetGames()
+    {
+        Console.Clear();
+        Console.WriteLine("Games History");
+        Console.WriteLine("----------------------------------------");
+        foreach (var game in games) Console.WriteLine(game);
+        Console.WriteLine("----------------------------------------\n");
+        Console.WriteLine("Press any key to go back to the main menu.");
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleMathGame/Menu.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleMathGame;
+
+internal class Menu
+{
+    private readonly GameEngine engine = new();
+
+    internal void ShowMenu(string name, DateTime date)
+    {
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine(
+            $"Hello {name}. It's {date}. This is your maths game. It's great that you're working on improving yourself.");
+        Console.WriteLine("\n");
+
+        var isGameOn = true;
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("""
+                              What game would you like to play today? Choose from the options below:
+                              V - View Previous Games
+                              A - Addition
+                              S - Subtraction
+                              M - Multiplication
+                              D - Division
+                              Q - Quit the program
+                              """);
+            Console.WriteLine("----------------------------------------");
+
+            var gameSelection = Console.ReadLine().ToLower().Trim();
+
+            switch (gameSelection)
+            {
+                case "a":
+                    engine.AdditionGame("Addition game");
+                    break;
+                case "d":
+                    engine.DivisionGame("Division game");
+                    break;
+                case "m":
+                    engine.MultiplicationGame("Multiplication game");
+                    break;
+                case "q":
+                    Console.WriteLine("Goodbye.");
+                    isGameOn = false;
+                    break;
+                case "s":
+                    engine.SubtractionGame("Subtraction game");
+                    break;
+                case "v":
+                    Helpers.GetGames();
+                    break;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    Environment.Exit(1);
+                    break;
+            }
+        } while (isGameOn);
+    }
+}
